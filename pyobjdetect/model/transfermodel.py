633c2106d08a7596c017c3c426f992839094a0f7
Neeth Kunnath <neeth.xavier@gmail.com>
2023-08-25 20:29:18 -0400

    training

--- pyobjdetect/model/transfermodel.py
+++ pyobjdetect/model/transfermodel.py
@@ -10,14 +10,16 @@ from torchvision import datasets, models, transforms
 
 import time
 import os
+from pathlib import Path
 from PIL import Image
 from tempfile import TemporaryDirectory
 
-from pyobjdetect.utils import misc, helpers
+from pyobjdetect.utils import misc, helpers, logutils, viz
 
 
 cudnn.benchmark = True
 
+BEST_MODEL_PARAMS_PATH = "best_model_params.pt"
 MEAN = [0.485, 0.456, 0.406]
 STD = [0.229, 0.224, 0.225]
 
@@ -65,18 +67,166 @@ def previz(inp):
     return inp
 
 
-def run(**kwargs):
-    from pyobjdetect.utils import logutils, viz
+def train_one_epoch(dataloaders, dataset_sizes, model, criterion, optimizer, device, phase):
+    running_loss = 0.0
+    running_corrects = 0
 
+    for inputs, labels in dataloaders[phase]:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # zero the parameters gradients
+        optimizer.zero_grad()
+
+        # forward
+        # track history if only in train
+        with torch.set_grad_enabled(phase == "train"):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, dim=1)
+            loss = criterion(outputs, labels)
+
+            # backwards + optimize only if in training phase
+            if phase == "train":
+                loss.backward()
+                optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+    epoch_loss = running_loss / dataset_sizes[phase]
+    epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+    logutils.info(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+
+    return epoch_loss, epoch_acc
+
+
+def train_model(
+    dataloaders, dataset_sizes, class_names, model, criterion, optimizer, scheduler, device, num_epochs=25
+):
+    since = time.time()
+    logutils.info("Training starting")
+
+    logutils.debug(f"classes in dataset: {class_names}")
+
+    logutils.debug(f"Running on {device}")
+
+    # Create a temporary directory to save training checkpoints
+    with TemporaryDirectory() as traindir:
+        logutils.debug(f"Training dir: {Path(traindir).absolute()}")
+
+        best_model_params_path = Path(os.path.join(traindir, BEST_MODEL_PARAMS_PATH)).absolute()
+
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
+
+        for epoch in range(num_epochs):
+            logutils.info(f"Epoch {epoch+1}/{num_epochs}")
+            logutils.secho("-" * 10)
+
+            for phase in ["train", "val"]:
+                if phase == "train":
+                    model.train()  # Set to train model
+                else:
+                    model.eval()  # Set to evaluation model
+
+                # Iterate over data
+                epoch_loss, epoch_acc = train_one_epoch(
+                    dataloaders=dataloaders,
+                    dataset_sizes=dataset_sizes,
+                    model=model,
+                    criterion=criterion,
+                    optimizer=optimizer,
+                    device=device,
+                    phase=phase,
+                )
+
+                # visualize_model(
+                #    model=model,
+                #    dataloader=dataloaders[phase],
+                #    class_names=class_names,
+                #    device=device,
+                #    title=f"epoch: {epoch+1}",
+                # )
+
+                if phase == "train":
+                    scheduler.step()
+
+                # deep copy the model
+                if phase == "val" and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(model.state_dict(), best_model_params_path)
+
+            print()
+
+        time_elapsed = time.time() - since
+        logutils.info(f"Training completed in {time_elapsed//60:.0f}m {time_elapsed%60:.0f}s")
+        logutils.info(f"Best val Acc: {best_acc:.4f}")
+        logutils.info(f"Best model saved at {best_model_params_path}")
+
+        model.load_state_dict(torch.load(best_model_params_path))
+
+    return model
+
+
+def visualize_model(model, dataloader, class_names, num_images=6, device="cpu", title=None):
+    if title is None:
+        title = ""
+    else:
+        title = f"{title} "
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+
+    fig, ax, _, _ = viz.subplots_n(num_images)
+    ax = ax.ravel()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloader):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                viz.matshow(
+                    ax[images_so_far],
+                    previz(inputs.cpu().data[j]),
+                    title=f"{title}prediction: {class_names[preds[j]]}",
+                )
+
+                images_so_far += 1
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+
+
+def run(**kwargs):
     logutils.VERBOSE = logutils.DEBUG
 
+    # data params
     bsize = kwargs.get("batch_size", 4)
     shuffle = kwargs.get("shuffle", True)
     num_workers = kwargs.get("num_workers", 4)
     gpu_num = kwargs.get("gpu_num", 0)
 
+    # train params
+    num_epochs = kwargs.get("num_epochs", 25)
+    lr = kwargs.get("lr", 0.001)
+    momentum = kwargs.get("momentum", 0.9)
+    lr_step_size = kwargs.get("lr_step_size", 7)
+    lr_gamma = kwargs.get("lr_gamma", 0.1)
+
     image_datasets = get_datasets()
+
     dataset_keys = [k for k in image_datasets.keys()]
+
+    class_names = image_datasets[dataset_keys[0]].classes
+    logutils.debug(f"classes in dataset: {class_names}")
+
     dataloaders = {
         x: torch.utils.data.DataLoader(image_datasets[x], batch_size=bsize, shuffle=shuffle, num_workers=num_workers)
         for x in dataset_keys
@@ -84,9 +234,6 @@ def run(**kwargs):
 
     dataset_sizes = {x: len(image_datasets[x]) for x in dataset_keys}
     logutils.debug(f"dataset size: {dataset_sizes}")
-
-    class_names = image_datasets[dataset_keys[0]].classes
-    logutils.debug(f"classes in dataset: {class_names}")
 
     device = torch.device(f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu")
     logutils.debug(f"Running on {device}")
@@ -99,6 +246,47 @@ def run(**kwargs):
 
     # display them
     viz.quickmatshow(out, title="Train sample")
+
+    ###############################
+    # Preparing pre trained model #
+    ###############################
+
+    model_ft = models.resnet18(weights="IMAGENET1K_V1")
+    numt_ftrs = model_ft.fc.in_features
+    # Here the size of each output sample is set to `num_classes`
+    model_ft.fc = nn.Linear(numt_ftrs, len(class_names))
+
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=lr, momentum=momentum)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer=optimizer_ft, step_size=lr_step_size, gamma=lr_gamma)
+
+    model_ft = train_model(
+        dataloaders=dataloaders,
+        dataset_sizes=dataset_sizes,
+        class_names=class_names,
+        model=model_ft,
+        criterion=criterion,
+        optimizer=optimizer_ft,
+        scheduler=exp_lr_scheduler,
+        device=device,
+        num_epochs=num_epochs,
+    )
+
+    visualize_model(
+        model=model_ft,
+        dataloader=dataloaders["val"],
+        class_names=class_names,
+        num_images=6,
+        device=device,
+        title="Best Model",
+    )
+
     viz.show()
 
 
